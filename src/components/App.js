ef972ed9b6e8734932fe078e862bd3f0fd3a67dc
Tim <tim.nguyen.cs@gmail.com>
2019-12-15 02:18:57 -0800

    simple file structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavigationBar from './NavigationBar/NavigationBar';
+import logo from '../assets/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Everything works!
         </p>
         <a
           className="App-link"
---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavigationBar() {
+  return (
+    <h1>This is a NavigationBar!</h1>
+  );
+}
+
+export default NavigationBar;
