92ee96c7e7514c98d69449503c8d6988c7c979e1
Tim <tim.nguyen.cs@gmail.com>
2019-12-20 15:29:18 -0800

    fixed navbar alignment

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -5,15 +5,18 @@ import { Navbar, Nav } from 'react-bootstrap';
 function NavigationBar() {
   return (
     <Navbar sticky="top" bg="light" expand="lg">
-      <Navbar.Brand className="mr-auto" href="#home">React - Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">React - Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">About</Nav.Link>
-        <Nav.Link href="#link">FAQ</Nav.Link>
-        <Nav.Link href="#link">Team</Nav.Link>
-        <Nav.Link href="#link">Sponsors</Nav.Link>
-        <Nav.Link href="#link">Contacts</Nav.Link>
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">About</Nav.Link>
+          <Nav.Link href="#link">FAQ</Nav.Link>
+          <Nav.Link href="#link">Team</Nav.Link>
+          <Nav.Link href="#link">Sponsors</Nav.Link>
+          <Nav.Link href="#link">Contacts</Nav.Link>
+        </Nav>
+
       </Navbar.Collapse>
 
     </Navbar>
