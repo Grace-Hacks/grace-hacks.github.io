e607841a79cb506488e5161f94d650f8662631ac
Tim <tim.nguyen.cs@gmail.com>
2019-12-21 02:05:01 -0800

    finished basic navbar

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -5,18 +5,17 @@ import { Link, animateScroll } from 'react-scroll';
 function NavigationBar() {
   return (
     <Navbar sticky="top" bg="light" expand="lg">
-      <Navbar.Brand href="#home">React - Bootstrap</Navbar.Brand>
+      <a onClick={() => animateScroll.scrollToTop() }>GraceHacks</a>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Link activeClass="active" to="about-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 }>About</Link>
-          <Link activeClass="active" to="faq-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 }>FAQ</Link>
-          <Link activeClass="active" to="team-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 }>Team</Link>
-          <Link activeClass="active" to="sponsors-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 }>Sponsors</Link>
-          <Link activeClass="active" to="contacts-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 }>Contacts</Link>
+        <Nav class="ml-auto">
+          <Link activeClass="active" to="about-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ marginRight: '2em', fontWeight: 'bold' }}>About</Link>
+          <Link activeClass="active" to="faq-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ marginRight: '2em', fontWeight: 'bold' }}>FAQ</Link>
+          <Link activeClass="active" to="team-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ marginRight: '2em', fontWeight: 'bold' }}>Team</Link>
+          <Link activeClass="active" to="sponsors-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ marginRight: '2em', fontWeight: 'bold' }}>Sponsors</Link>
+          <Link activeClass="active" to="contacts-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ fontWeight: 'bold' }}>Contacts</Link>
         </Nav>
       </Navbar.Collapse>
-
     </Navbar>
   );
 }
