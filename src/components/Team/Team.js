8f36c3cb5e76ed722081a23604d2e348d3d2fd5b
shivikasiva <shivikasiva@gmail.com>
2020-01-12 14:16:31 -0800

    Revert "Test PR/Update Team.js"

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,37 +2,7 @@ import React from 'react';
 
 function Team() {
   return (
-
-    <Container id="team-section">
-        <Row>
-            <Col>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Shivika Sivakumar</Card.Title>
-                        <Card.Text>
-                            Tech Team Lead
-                        </Card.Text>
-                        <Button variant="primary">LinkedIn</Button>
-                    </Card.Body>
-                </Card>
-            </Col>
-
-            <Col>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Tim Nguyen</Card.Title>
-                        <Card.Text>
-                            Tech Team Member
-                        </Card.Text>
-                        <Button variant="primary">LinkedIn</Button>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Row>
-    </Container>
-
+     <div id="team-section">"Team" Section!</div>
   );
 }
 
