4e9cb427435343e8b9b86c64200f5ddd04a845cf
Tim <tim.nguyen.cs@gmail.com>
2020-02-15 23:32:31 -0800

    changes to pictures

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -13,9 +13,9 @@ function Team() {
 
         <Row style={{paddingTop:"20px"}}>
             <Col>
-                <Card style={{ width: '18rem' }}>
+                <Card className="mx-auto"style={{ width: '18rem' }}>
                     <Card.Img variant="top"/>
-                    <img src={require('./Saniya.JPG')} width = "287px" height = "300px" alt="Card image"/>
+                    <img src={require('./Saniya.png')} width = "287px" height = "300px" alt="Card image"/>
                     <Card.Body>
                     <Card.Title><Button variant="primary">Saniya Doshi</Button></Card.Title>
                         <Card.Text>
@@ -24,87 +24,88 @@ function Team() {
                     </Card.Body>
                 </Card>
             </Col>
-
-            <Col>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top"/>
-                    <img src={require('./Navya.jpeg')} width = "287px" height = "300px" alt="Card image"/>
-                    <Card.Body>
-                        <Card.Title><Button variant="primary">Navya Rao</Button></Card.Title>
-                        <Card.Text>
-                            Co-Founder and PR Lead
-                        </Card.Text>
-                        {/* <Card.Title> 
-                            <Link to='/https://www.google.com/'>
-                                <Button variant="primary">Navya Rao</Button>
-                            </Link>
-                            <Card.Text>
-                                Co-founder and PR Team Lead
-                            </Card.Text>
-                        </Card.Title> */}
-                    </Card.Body>
-                </Card>
-            </Col>
-
-            <Col>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top"/>
-                    <img src={require('./Shivika.png')} width = "287px" height = "300px" alt="Card image"/>
-                    <Card.Body>
-                    <Card.Title><Button variant="primary">Shivika Sivakumar</Button></Card.Title>
-                        <Card.Text>
-                            Co-Founder and Tech Team Lead
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>
         </Row>
 
-        <Row style={{paddingTop:"20px"}}>
-            <Col>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top"/>
-                    <img src={require('./Kelly.jpeg')} width = "287px" height = "300px" alt="Card image"/>
-                    <Card.Body>
-                    <Card.Title><Button variant="primary">Kelly Fesler</Button></Card.Title>
-                        <Card.Text>
-                            Logistics Team Lead
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>
+        <Row style={{ paddingTop: "20px" }}>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top"/>
+                  <img src={require('./Navya.png')} width = "287px" height = "300px" alt="Card image"/>
+                  <Card.Body>
+                    <Card.Title><Button variant="primary">Navya Rao</Button></Card.Title>
+                    <Card.Text>
+                      Co-Founder and PR Lead
+                    </Card.Text>
+                    {/* <Card.Title>
+                      <Link to='/https://www.google.com/'>
+                        <Button variant="primary">Navya Rao</Button>
+                      </Link>
+                      <Card.Text>
+                        Co-founder and PR Team Lead
+                      </Card.Text>
+                    </Card.Title> */}
+                  </Card.Body>
+            </Card>
+          </Col>
 
-            <Col>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top"/>
-                    <img src={require('./Namratha.jpeg')} width = "287px" height = "300px" alt="Card image"/>
-                    <Card.Body>
-                    <Card.Title><Button variant="primary">Namratha Prithviraj</Button></Card.Title>
-                        <Card.Text>
-                            Sponsorship Team Lead
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </Col>
+          <Col>
+              <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top"/>
+                  <img src={require('./Shivika.png')} width = "287px" height = "300px" alt="Card image"/>
+                  <Card.Body>
+                  <Card.Title><Button variant="primary">Shivika Sivakumar</Button></Card.Title>
+                      <Card.Text>
+                          Co-Founder and Tech Team Lead
+                      </Card.Text>
+                  </Card.Body>
+              </Card>
+          </Col>
 
-            <Col>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top"/>
+                <img src={require('./Kelly.png')} width = "287px" height = "300px" alt="Card image"/>
+                <Card.Body>
+                <Card.Title><Button variant="primary">Kelly Fesler</Button></Card.Title>
+                    <Card.Text>
+                        Logistics Team Lead
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row style={{ paddingTop: "20px" }}>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top"/>
+                <img src={require('./Namratha.png')} width = "287px" height = "300px" alt="Card image"/>
+                <Card.Body>
+                <Card.Title><Button variant="primary">Namratha Prithviraj</Button></Card.Title>
+                    <Card.Text>
+                        Sponsorship Team Lead
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+          </Col>
+
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top"/>
+                  <img src={require('./Sathya.png')} width = "287px" height = "300px" alt="Card image"/>
+                  <Card.Body>
                     <Card.Title><Button variant="primary">Sathya Gadhiraju</Button></Card.Title>
-                        <Card.Text>
-                            PR Team Member
-                        </Card.Text>
+                      <Card.Text>
+                        PR Team Member
+                      </Card.Text>
                     </Card.Body>
                 </Card>
             </Col>
-        </Row>
 
-        <Row style={{paddingTop:"20px"}}>
             <Col>
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top"/>
-                    <img src={require('./Tim.jpeg')} width = "287px" height = "300px" alt="Card image"/>
+                    <img src={require('./Tim.png')} width = "287px" height = "300px" alt="Card image"/>
                     <Card.Body>
                         <Card.Title><Button variant="primary">Tim Nguyen</Button></Card.Title>
                         <Card.Text>
@@ -113,11 +114,13 @@ function Team() {
                     </Card.Body>
                 </Card>
             </Col>
+          </Row>
 
+          <Row style={{ paddingTop: "20px" }}>
             <Col>
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top"/>
-                    <img src={require('./Desi.jpeg')} width = "287px" height = "300px" alt="Card image"/>
+                    <img src={require('./Desi.png')} width = "287px" height = "300px" alt="Card image"/>
                     <Card.Body>
                         <Card.Title><Button variant="primary">Desiderio Ascencio</Button></Card.Title>
                         <Card.Text>
@@ -126,11 +129,11 @@ function Team() {
                     </Card.Body>
                 </Card>
             </Col>
-            
+
             <Col>
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top"/>
-                    <img src={require('./Anu.jpeg')} width = "287px" height = "300px" alt="Card image"/>
+                    <img src={require('./Anu.png')} width = "287px" height = "300px" alt="Card image"/>
                     <Card.Body>
                     <Card.Title><Button variant="primary">Anupreet Singh</Button></Card.Title>
                         <Card.Text>
@@ -139,9 +142,21 @@ function Team() {
                     </Card.Body>
                 </Card>
             </Col>
-        </Row>
-    </Container>
 
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top"/>
+                  <img src={require('./Simran.png')} width = "287px" height = "300px" alt="Card image"/>
+                  <Card.Body>
+                    <Card.Title><Button variant="primary">Simran Gunsi</Button></Card.Title>
+                    <Card.Text>
+                      Lead Designer
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+      </Container>
   );
 }
 
