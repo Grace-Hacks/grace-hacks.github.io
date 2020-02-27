c2059a81b4cb4510b24246b1f39da5fdc8214002
Tim <tim.nguyen.cs@gmail.com>
2020-02-27 00:25:40 -0800

    nav section

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link, animateScroll } from 'react-scroll';
+import './NavigationBar.css';
 
 function NavigationBar() {
   return (
-    <Navbar sticky="top" bg="light" expand="lg">
-      <a onClick={() => animateScroll.scrollToTop() }>GraceHacks</a>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav class="ml-auto">
-          <Link activeClass="active" to="about-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ marginRight: '2em', fontWeight: 'bold' }}>About</Link>
-          <Link activeClass="active" to="faq-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ marginRight: '2em', fontWeight: 'bold' }}>FAQ</Link>
-          <Link activeClass="active" to="team-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ marginRight: '2em', fontWeight: 'bold' }}>Team</Link>
-          <Link activeClass="active" to="sponsors-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ marginRight: '2em', fontWeight: 'bold' }}>Sponsors</Link>
-          <Link activeClass="active" to="contacts-section" spy={ true } smooth={ true } offset={ -70 } duration= { 500 } style={{ fontWeight: 'bold' }}>Contacts</Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div class="navigation-container">
+      <a class="nav-title"href="https://grace-hacks.github.io/">GRACEHACKS</a>
+      <div class="nav-info">
+        <div class="nav-faq">FAQ</div>
+        <div class="nav-apply">APPLY</div>
+        <div class="nav-icon"></div>
+      </div>
+    </div>
   );
 }
 
