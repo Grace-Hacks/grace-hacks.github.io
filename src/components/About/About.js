5165ccb126108e657fd58561d82bb07fa2cbacce
Tim <tim.nguyen.cs@gmail.com>
2020-02-27 00:25:12 -0800

    about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import './About.css';
 
 function About() {
   return (
-    <Card id="about-section" className="mx-auto col-md-9" border="primary">
-      <Card.Header as="h5">Our Mission</Card.Header>
-      <Card.Body>
-        <Card.Title>Create With Confidence.</Card.Title>
-        <Card.Text>The aim of GraceHacks is to foster a welcoming and supportive hackathon environment for womxn and non-binary students. GraceHacks strives to empower these underrepresented groups and ensure that they have the confidence to create without inherent bias or sexism</Card.Text>
-      </Card.Body>
-    </Card>
+    <div class="about-container">
+      <div class="about-title">Create With Confidence</div>
+      <div class="about-description">The aim of GraceHacks is to foster a welcoming and supportive hackathon environment for women and non-binary students. GraceHacks strives to empower these underrepresented groups and ensure that they have the confidence to create without inherent bias or sexism.</div>
+    </div>
   );
 }
 
