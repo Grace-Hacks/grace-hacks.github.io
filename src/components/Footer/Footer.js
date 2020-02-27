55f5001b09b936fa18ee117fd6081e9ba94c8445
Tim <tim.nguyen.cs@gmail.com>
2020-02-27 00:25:44 -0800

    footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import './Footer.css';
 
 function Footer() {
   return (
-    <Row style={{ margin: 0 }}>
-      <Col style={{textAlign: 'left'}}>Made with <span role='img' aria-label='hearts'>❤️❤️❤️</span> by <span role='img' aria-label='snails'>🐌🐌🐌</span> in Santa Cruz</Col>
-      <Col style={{textAlign: 'right'}}>© 2020 GraceHacks. All rights reserved.</Col>
-    </Row>
+    <div class="footer-container">
+      <div class="footer-contact">CONTACT US</div>
+      <div class="footer-social">
+        <div class="footer-icon"></div>
+        <div class="footer-icon middle"></div>
+        <div class="footer-icon"></div>
+      </div>
+    </div>
   );
 }
 
