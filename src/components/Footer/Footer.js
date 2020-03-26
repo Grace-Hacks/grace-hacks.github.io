c0fec285d0ef28c6f089469aabf3e29e1797cf32
Tim <tim.nguyen.cs@gmail.com>
2020-03-26 14:37:03 -0700

    updated with images and new footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Footer.css';
 
+import instagram from '../../assets/instagram.png';
+import facebook from '../../assets/facebook.png';
+import linkedin from '../../assets/linkedin.png';
+
 function Footer() {
   return (
     <div class="footer-container">
       <div class="footer-contact">CONTACT US</div>
       <div class="footer-social">
-        <div class="footer-icon"></div>
-        <div class="footer-icon middle"></div>
-        <div class="footer-icon"></div>
+        <a href="https://www.instagram.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="footer-icon" src={instagram}  alt="instagram"/></a>
+        <a href="https://www.facebook.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="footer-icon middle" src={facebook}  alt="facebook"/></a>
+        <a href="https://www.linkedin.com/company/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="footer-icon" src={linkedin}  alt="linkedin"/></a>
+      </div>
+      <div class="footer-message">
+        <div>Made with <span role='img' aria-label='hearts'>❤️❤️❤️</span> by <span role='img' aria-label='snails'>🐌🐌🐌</span> in Santa Cruz.</div>
+        <div>© 2020 GraceHacks. All rights reserved.</div>
       </div>
     </div>
   );
