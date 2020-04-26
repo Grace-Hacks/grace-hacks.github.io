f4d9ffadfb62bb11141f3be10f43ad9bd67312d9
Tim <tim.nguyen.cs@gmail.com>
2020-04-25 23:24:38 -0700

    email update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import './Footer.css';
 import instagram from '../../assets/instagram.png';
 import facebook from '../../assets/facebook.png';
 import linkedin from '../../assets/linkedin.png';
+import email from '../../assets/email.png'
 
 function Footer() {
   return (
@@ -11,8 +12,9 @@ function Footer() {
       <div class="footer-contact">CONTACT US</div>
       <div class="footer-social">
         <a href="https://www.instagram.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="footer-icon" src={instagram}  alt="instagram"/></a>
-        <a href="https://www.facebook.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="footer-icon middle" src={facebook}  alt="facebook"/></a>
-        <a href="https://www.linkedin.com/company/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="footer-icon" src={linkedin}  alt="linkedin"/></a>
+        <a href="https://www.facebook.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="footer-icon left" src={facebook}  alt="facebook"/></a>
+        <a href="https://www.linkedin.com/company/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="footer-icon right" src={linkedin}  alt="linkedin"/></a>
+        <a href="mailto:ucsc.gracehacks@gmail.com" target="_blank" rel="noopener noreferrer"><img class="footer-icon" src={email} alt="email"/></a>
       </div>
       <div class="footer-message">
         <div>Made with <span role='img' aria-label='hearts'>❤️❤️❤️</span> by <span role='img' aria-label='snails'>🐌🐌🐌</span> in Santa Cruz.</div>
