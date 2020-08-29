a9db43a9299f0a1ddb532a5216dc977a8360c4ec
Sarah Avila <sttavila@gmail.com>
2020-08-28 23:00:32 -0700

    tweak some styles and try to match title styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,16 +6,17 @@ function Header() {
   return (
     <div class="header-container">
       <img class="header-image" src={GraceHacks}></img>
-      <div className="title">
-        <div id="letter">U</div>
-        <div id="letter">C</div>
-        <div id="letter">S</div>
-        <div id="letter">C</div>
-        <div id="word">GraceHacks</div>
+      <div class="title">
+        <div class="letterHeader">U</div>
+        <div class="letterHeader">C</div>
+        <div class="letterHeader">S</div>
+        <div class="letterHeader">C</div>
+        <div class="spacer"></div>
+        <div class="word">GraceHacks</div>
       </div>
-      <div id="event"> October 17th & 18th - Online @ 11AM PST</div>
+      <div class="event"> October 17th & 18th - Online @ 11AM PST</div>
       <a href = "https://tinyurl.com/GH-app" target="_blank" rel="noopener noreferrer" class="button">
-        <div id="register"> REGISTER </div>
+        <div class="register"> REGISTER </div>
       </a>
     </div>
   );
