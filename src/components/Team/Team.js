861958dcb8d7c5f48898b470c8f63d9d00e13690
Sarah Avila <sttavila@gmail.com>
2020-09-01 21:27:24 -0700

    fix team heading text

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,16 +5,18 @@ function Team() {
   return (
     <div class="team-container">
       <div class="team-title">
-        <div class="letter">T</div>
-        <div class="letter">H</div>
-        <div class="letter">E</div> 
-
+        <div class="word">
+          <div class="letter">T</div>
+          <div class="letter">H</div>
+          <div class="letter">E</div> 
+        </div>
         <div class="spacer"></div>
-        
-        <div class="letter">T</div>
-        <div class="letter">E</div>
-        <div class="letter">A</div>
-        <div class="letter">M</div>
+        <div class="word">
+          <div class="letter">T</div>
+          <div class="letter">E</div>
+          <div class="letter">A</div>
+          <div class="letter">M</div>
+        </div>
       </div>
       <div class="team-row">
         <div class="member">
