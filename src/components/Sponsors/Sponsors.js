b295da80346d7db018c6a7abe2bb51e455eca1ce
chuglies <brinaemanuel@gmail.com>
2020-09-15 20:51:28 -0700

    made logos bigger for better readability

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -15,43 +15,121 @@ function Sponsors() {
         <div class="letter">S</div>
       </div>
 
-      <div class="box">We are still looking for sponsors for GraceHacks 2020. Please reach out to us if you're 
-      interested in helping make this hackathon a success.</div>
+      <div class="box">
+        We are still looking for sponsors for GraceHacks 2020. Please reach out
+        to us if you're interested in helping make this hackathon a success.
+      </div>
 
       <div class="bg-button">SPONSOR GRACE HACKS TODAY!</div>
 
       <div class="sponsors-row">
-        <img src={require("./images/CDKGlobal.svg")} class="icon x-large-logo" alt="CDKGlobal" />
-        <img src={require("./images/Salesforce.svg")} class="icon x-large-logo" alt="Salesforce" />
-        <img src={require("./images/Nutanix.svg")} class="icon x-large-logo" alt="Nutanix" />
-        <img src={require("./images/CBT.svg")} class="icon x-large-logo" alt="CBTech" />
+        <img
+          src={require("./images/CDKGlobal.svg")}
+          class="icon x-large-logo"
+          alt="CDKGlobal"
+        />
+        <img
+          src={require("./images/Salesforce.svg")}
+          class="icon x-large-logo"
+          alt="Salesforce"
+        />
+        <img
+          src={require("./images/Nutanix.svg")}
+          class="icon x-large-logo"
+          alt="Nutanix"
+        />
+        <img
+          src={require("./images/CBT.svg")}
+          class="icon CBT-logo"
+          alt="CBTech"
+        />
       </div>
 
       <div class="sponsors-row">
-        <img src={require("./images/Linode.svg")} class="icon large-logo" alt="Linode" />
-        <img src={require("./images/Balsamiq.svg")} class="icon large-logo" alt="Balsamiq" />
-        <img src={require("./images/RStudio.svg")} class="icon large-logo" alt="RStudio" />
+        <img
+          src={require("./images/Linode.svg")}
+          class="icon large-logo"
+          alt="Linode"
+        />
+        <img
+          src={require("./images/Balsamiq.svg")}
+          class="icon large-logo"
+          alt="Balsamiq"
+        />
+        <img
+          src={require("./images/RStudio.svg")}
+          class="icon large-logo"
+          alt="RStudio"
+        />
       </div>
 
       <div class="sponsors-row">
-        <img src={require("./images/MouserElectronics.svg")} class="icon small-logo" alt="Mouser Electronics" />
-        <img src={require("./images/Girls-Make-Games.svg")} class="icon small-logo" alt="Girls Make Games" />
-        <img src={require("./images/Makey-Makey.svg")} class="icon small-logo" alt="Makey Makey" />
-        <img src={require("./images/Desmos.svg")} class="icon small-logo" alt="Desmos" />
-        <img src={require("./images/Heroku.svg")} class="icon small-logo" alt="Heroku" />
+        <img
+          src={require("./images/MouserElectronics.svg")}
+          class="icon small-logo"
+          alt="Mouser Electronics"
+        />
+        <img
+          src={require("./images/Girls-Make-Games.svg")}
+          class="icon small-logo"
+          alt="Girls Make Games"
+        />
+        <img
+          src={require("./images/Makey-Makey.svg")}
+          class="icon small-logo"
+          alt="Makey Makey"
+        />
+        <img
+          src={require("./images/Desmos.svg")}
+          class="icon small-logo"
+          alt="Desmos"
+        />
+        <img
+          src={require("./images/Heroku.svg")}
+          class="icon small-logo"
+          alt="Heroku"
+        />
       </div>
 
       <div class="sponsors-row">
-        <img src={require("./images/Sticker-Giant.svg")} class="icon small-logo" alt="Sticker Giant" />
-        <img src={require("./images/Kingston.svg")} class="icon medium-logo" alt="Kingston" />
-        <img src={require("./images/Sentry.svg")} class="icon small-logo" alt="Sentry" />
-        <img src={require("./images/HVMN.svg")} class="icon small-logo" alt="HVMN" />
-        <img src={require("./images/CloudSploit.svg")} class="icon small-logo" alt="CloudSploit" />
+        <img
+          src={require("./images/Sticker-Giant.svg")}
+          class="icon small-logo"
+          alt="Sticker Giant"
+        />
+        <img
+          src={require("./images/Kingston.svg")}
+          class="icon medium-logo"
+          alt="Kingston"
+        />
+        <img
+          src={require("./images/Sentry.svg")}
+          class="icon small-logo"
+          alt="Sentry"
+        />
+        <img
+          src={require("./images/HVMN.svg")}
+          class="icon small-logo"
+          alt="HVMN"
+        />
+        <img
+          src={require("./images/CloudSploit.svg")}
+          class="icon small-logo"
+          alt="CloudSploit"
+        />
       </div>
 
       <div class="sponsor-row">
-        <img src={require("./images/AI-Camp.svg")} class="icon x-small-logo" alt="AI Camp" />
-        <img src={require("./images/SCW.svg")} class="icon x-small-logo" alt="Santa Cruz Works" />
+        <img
+          src={require("./images/AI-Camp.svg")}
+          class="icon x-small-logo"
+          alt="AI Camp"
+        />
+        <img
+          src={require("./images/SCW.svg")}
+          class="icon x-small-logo"
+          alt="Santa Cruz Works"
+        />
       </div>
     </div>
   );
