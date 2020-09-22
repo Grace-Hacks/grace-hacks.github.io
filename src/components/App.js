c4a0ac041c983c5c99985e8a262db3a0f4398e69
Tim <tim.nguyen.cs@gmail.com>
2020-09-22 12:06:23 -0700

    removed deprecated contacts section

--- src/components/App.js
+++ src/components/App.js
@@ -17,30 +17,10 @@ function App() {
       <div className="app-container">
         <NavigationBar />
         <Header />
-
-        {/* <script src="https://embed.typeform.com/embed.js"></script>
-
-        <Jumbotron style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '75vh', textAlign: "left" }}>
-          <h1>GraceHacks @ UCSC</h1>
-          <p>UC Santa Cruz's premier all-female and non-binary hackathon.</p>
-
-          <p>
-            <Button variant="primary">
-              REGISTER!
-            </Button>
-          </p>
-
-          <NewWindow>
-            window.open(<ReactTypeformEmbed url="https://shivikasivakumar.typeform.com/to/fmrvAY"/>)
-          </NewWindow>
-
-        </Jumbotron> */}
-
         <About />
         <FAQ />
         <Team />
         <Sponsors />
-        {/*<Contacts />  */}
         <Footer />
       </div>
     );
