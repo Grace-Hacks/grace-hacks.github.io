6cbeafebf664c186c7fc56d5b79048adb9db9890
Sarah Avila <sttavila@gmail.com>
2020-09-22 19:00:17 -0700

    slight margin change to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,13 +12,13 @@ function Footer() {
       <div class="contact">CONTACT US</div>
       <div class="social">
         <a href="https://www.instagram.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="icon" src={instagram}  alt="instagram"/></a>
-        <a href="https://www.facebook.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="icon left" src={facebook}  alt="facebook"/></a>
-        <a href="https://www.linkedin.com/company/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="icon right" src={linkedin}  alt="linkedin"/></a>
+        <a href="https://www.facebook.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="icon" src={facebook}  alt="facebook"/></a>
+        <a href="https://www.linkedin.com/company/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="icon" src={linkedin}  alt="linkedin"/></a>
         <a href="mailto:ucsc.gracehacks@gmail.com" target="_blank" rel="noopener noreferrer"><img class="icon" src={email} alt="email"/></a>
       </div>
       <div class="message">
-        <div class="made">Made with <span role='img' aria-label='hearts'>❤️❤️❤️</span> by <span role='img' aria-label='snails'>🐌🐌🐌</span> in Santa Cruz.</div>
-        <div class="rights">© 2020 GraceHacks. All rights reserved.</div>
+        <div class="text">Made with <span role='img' aria-label='hearts'>❤️❤️❤️</span> by <span role='img' aria-label='snails'>🐌🐌🐌</span> in Santa Cruz.</div>
+        <div class="text">© 2020 GraceHacks. All rights reserved.</div>
       </div>
     </div>
   );
