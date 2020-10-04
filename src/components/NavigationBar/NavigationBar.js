50cec10ec12e3d424934da1e03777e26923d28e6
Tim <tim.nguyen.cs@gmail.com>
2020-10-04 15:14:18 -0700

    revised navigation for desktop view

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
 import './NavigationBar.scss';
-import instagram from '../../assets/instagram.png'
+import GraceHacks from '../../assets/GraceHacks.png'
+
+const useStyles = makeStyles({
+  anchor: {
+    fontSize: "28px",
+    marginRight: "20px",
+    fontFamily: "inherit",
+    color: "inherit",
+    padding: "0px"
+  },
+  register: {
+    fontSize: "21px",
+    fontFamily: "inherit",
+    color: "#413f3f"
+  }
+});
 
 function NavigationBar() {
   return (
-    <div class="navigation-container">
-      <a class="nav-title"href="https://grace-hacks.github.io/">GRACEHACKS</a>
-      <div class="info">
-        <a href="https://tinyurl.com/GH-app" target="_blank" rel="noopener noreferrer" class="apply">APPLY</a>
-        <a href="https://www.instagram.com/gracehacks/" target="_blank" rel="noopener noreferrer"><img class="icon" src={instagram}  alt="instagram"/></a>
-      </div>
-    </div>
+    <AppBar position="fixed">
+      <Toolbar class="navigation-container">
+        <div class="logo">
+          <img class="image" src={GraceHacks}></img>
+          <a class="title" href="https://grace-hacks.github.io/">GraceHacks</a>
+        </div>
+        <div class="info">
+          <Button variant="outline" className={useStyles().anchor}><AnchorLink href="#mission">MISSION</AnchorLink></Button>
+          <Button variant="outline" className={useStyles().anchor}><AnchorLink href="#faq">FAQ</AnchorLink></Button>
+          <Button variant="outline" className={useStyles().anchor}><AnchorLink href='#sponsors'>SPONSOR</AnchorLink></Button>
+          <Button variant="contained" href="https://tinyurl.com/GH-app">REGISTER</Button>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
