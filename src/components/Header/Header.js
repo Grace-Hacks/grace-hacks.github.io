3be4512d68176b1b25a6116eab5dd016d663a0c1
Sarah Avila <sttavila@gmail.com>
2020-10-08 16:20:57 -0700

    make button default and change to px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Header.scss';
 import GraceHacks from '../../assets/GraceHacks.png'
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 function Header() {
+
+  const useStyles = makeStyles((theme) => ({
+    register: {
+        fontSize: "25px",
+        fontFamily: "inherit",
+        color: "#413f3f !important",
+        width: "185px",
+        marginLeft: "800px"
+      }
+    }));
+    
   return (
     <div class="header-container">
       <img class="image" src={GraceHacks}></img>
@@ -15,9 +27,7 @@ function Header() {
         <div class="gracehacks">GraceHacks</div>
       </div>
       <div class="event"> October 17th-18th 11AM PST</div>
-      <a href = "https://tinyurl.com/GH-app" target="_blank" rel="noopener noreferrer" class="button">
-        <Button variant="contained" class="register">REGISTER</Button>
-      </a>
+      <Button variant="contained" href="https://tinyurl.com/GH-app" className={useStyles().register}>REGISTER</Button>
     </div>
   );
 }
