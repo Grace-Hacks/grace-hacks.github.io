c540237a7f2963be51e33b5345fb5122078f8bdd
Tim <tim.nguyen.cs@gmail.com>
2020-10-09 10:24:43 -0700

    design and preliminary countdown

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Countdown from 'react-countdown';
+
+import Keyboard from '../../assets/keyboard.png'
 import './About.scss';
 
 function About() {
   return (
     <div class="about-container" id="mission">
-      <div class="title">Create With Confidence</div>
-      <div class="subtitle">UC Santa Cruz's premier all-female and non-binary hackathon.</div>
-      <div class="description">The aim of GraceHacks is to foster a welcoming and supportive hackathon environment for women and non-binary students. GraceHacks strives to empower these underrepresented groups and ensure that they have the confidence to create without inherent bias or sexism.</div>
+      <img class="image" src={Keyboard}></img>
+      <div class="info">
+        <div class="title">Create with Confidence</div>
+        <div class="description">The aim of GraceHacks is to foster a welcoming and supportive hackathon environment for women and non-binary students. GraceHacks strives to empower these underrepresented groups and ensure that they have the confidence to create without inherent bias or sexism.</div>
+        <Countdown date={"2020-10-17T18:00:00Z"} daysInHours="true" /> Hours
+      </div>
     </div>
   );
 }
