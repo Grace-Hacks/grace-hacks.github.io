ca73133ad9d0b4afaa0f25568217dec4b67a4d88
Tim <tim.nguyen.cs@gmail.com>
2020-10-10 14:29:19 -0700

    mild updates and optimizations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,18 +8,14 @@ function Header() {
 
   const useStyles = makeStyles((theme) => ({
     register: {
-        display: "flex",
-        // justifyContent: "center",
-        // alignItems: "center",
-        fontSize: "25px",
-        fontFamily: "inherit",
-        color: "#413f3f !important",
-        width: "185px",
-        marginLeft: "20px",
-        flexWrap: "wrap"
-      }
-    }));
-    
+      fontSize: "25px",
+      fontFamily: "inherit",
+      color: "#413f3f !important",
+      width: "185px",
+      marginLeft: "20px",
+    }
+  }));
+
   return (
     <div class="header-container">
       <div class="left-column">
@@ -32,9 +28,7 @@ function Header() {
         </div>
       </div>
       <div class="right-column">
-        <div class="title">
-          <div class="gracehacks">GraceHacks</div>
-        </div>
+        <div class="gracehacks">GraceHacks</div>
         <div class="event"> October 17th-18th 11AM PST</div>
         <Button variant="contained" href="https://tinyurl.com/GH-app" className={useStyles().register}>REGISTER</Button>
       </div>
