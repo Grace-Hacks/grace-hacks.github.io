a66c29f540fd5b6e329bd8f4148d750c34342700
mpadil18 <mpadil18@ucsc.edu>
2020-10-10 02:40:22 -0700

    update

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import './FAQ.css';
+import './FAQ.scss';
+
+import { withStyles } from '@material-ui/core/styles';
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+
+import { ExpandMore } from '@material-ui/icons';
+import { AccordionSummary } from '@material-ui/core';
+
+const Accordion = withStyles({
+  root: {
+    width: '69%',
+    borderRadius: '25px',
+    margin: 'auto',
+    margin: '20px 0',
+
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+
+    '&:before': {
+      display: 'none',
+    },
+
+    '&$expanded': {
+      margin: '20px 0',
+    },
+  },
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    fontSize: '22px',
+    lineHeight: '30px',
+    fontFamily: '"Nunito", sans-serif',
+    color: '#FFFFFF',
+  },
+}))(MuiAccordionDetails);
 
 function FAQ() {
-  function toggleState(id) {
-    if (document.getElementById(id).style.display === '') {
-      document.getElementById(id).style.display = 'block';
-    }
-
-    if (document.getElementById(id).style.display === 'none') {
-      document.getElementById(id).style.display = 'block';
-    } else if (document.getElementById(id).style.display === 'block') {
-      document.getElementById(id).style.display = 'none';
-    }
-  };
-
   return (
   <div>
     <div class="page-title">
@@ -21,37 +49,42 @@ function FAQ() {
       <div class="letter">A</div>
       <div class="letter">Q</div>
     </div>
-    <div class="faq-section">
-      <div class="faq-row" style={{minHeight: "515px"}}>
-        <div class="module">
-          <div class="faq-title" onClick={() => {toggleState("one")}}><div class= 'ques'>What is a hackathon?</div> <img src={require("./vector.png")} class="arrow"/></div>
-          <div class="faq-answer" id="one">A hackathon is a place where you can collaborate with others and express your creativity to create a product that solves some real-world problem. At GraceHacks, students of all different backgrounds and experiences will gather together to develop an idea and realize that solution all within the span of 12 hours!</div>
-        </div>
-        <div class="module">
-          <div class="faq-title" onClick={() => {toggleState("two")}}><div class= 'ques'>Who can attend?</div> <img src={require("./vector.png")} class="arrow"/></div>
-          <div class="faq-answer" id="two">All female-identifying or non-binary college students (both undergraduates and graduates) are invited to attend, even with no prior experience necessary!</div>
-        </div>
-        <div class="module">
-          <div class="faq-title" onClick={() => {toggleState("three")}}><div class= 'ques'>What if I don’t have a team or idea?</div> <img src={require("./vector.png")} class="arrow"/></div>
-          <div class="faq-answer" id="three">No worries! We’ll have a short activity at the beginning of the hackathon, where you can meet others who you might want to collaborate with :-)</div>
-        </div>
-        <div class="module">
-          <div class="faq-title" onClick={() => {toggleState("four")}}><div class= 'ques'>What if I don’t know how to code?</div> <img src={require("./vector.png")} class="arrow"/></div>
-          <div class="faq-answer" id="four">GraceHacks is a wonderful place to get your feet wet in software development! Not a technical major? Don’t worry ~ the best hackathon teams  delicately balance graphic designers, project managers, and business associates as well!</div>
-        </div>
-        <div class="module">
-          <div class="faq-title" onClick={() => {toggleState("five")}}><div class= 'ques'>How do I attend?</div> <img src={require("./vector.png")} class="arrow"/></div>
-          <div class="faq-answer" id="five">This event is completely virtual. As long as you have internet connection and accessability to Zoom you are good to go!</div>
-        </div>
-        <div class="module">
-          <div class="faq-title" onClick={() => {toggleState("six")}}><div class= 'ques'>What can I create?</div> <img src={require("./vector.png")} class="arrow"/></div>
-          <div class="faq-answer" id="six">Anything your heart desires! Hardware products, software applications, gaming interfaces ~ if you can dream it, you can build it!</div>
-        </div>
-        <div class="module">
-          <div class="faq-title" onClick={() => {toggleState("seven")}}><div class= 'ques'>How much does it cost?</div> <img src={require("./vector.png")} class="arrow"/></div>
-          <div class="faq-answer" id="seven">GraceHacks will be absolutely FREE! We’ll provide food, swag, caffeine, WiFi, and a collaborative workspace to get your projects up and running quickly!</div>
-        </div>
-      </div>
+
+    <div>
+    <Accordion>
+      <AccordionSummary  expandIcon={<ExpandMore />}>What is a hackathon?</AccordionSummary>
+      <AccordionDetails>A hackathon is a place where you can collaborate with others and express your creativity to create a product that solves some real-world problem. At GraceHacks, students of all different backgrounds and experiences will gather together to develop an idea and realize that solution all within the span of 12 hours!</AccordionDetails>
+    </Accordion>
+
+    <Accordion>
+      <AccordionSummary  expandIcon={<ExpandMore />}>Who can attend?</AccordionSummary>
+      <AccordionDetails>All female-identifying or non-binary college students (both undergraduates and graduates) are invited to attend, even with no prior experience necessary!</AccordionDetails>
+    </Accordion>
+
+    <Accordion>
+      <AccordionSummary  expandIcon={<ExpandMore />}>What if I don’t have a team or idea?</AccordionSummary>
+      <AccordionDetails>No worries! We’ll have a short activity at the beginning of the hackathon, where you can meet others who you might want to collaborate with :-)</AccordionDetails>
+    </Accordion>
+
+    <Accordion>
+      <AccordionSummary  expandIcon={<ExpandMore />}>What if I don’t know how to code?</AccordionSummary>
+      <AccordionDetails>GraceHacks is a wonderful place to get your feet wet in software development! Not a technical major? Don’t worry ~ the best hackathon teams delicately balance graphic designers, project managers, and business associates as well!</AccordionDetails>
+    </Accordion>
+
+    <Accordion>
+      <AccordionSummary  expandIcon={<ExpandMore />}>How do I attend?</AccordionSummary>
+      <AccordionDetails>This event is completely virtual. As long as you have internet connection and accessability to Zoom you are good to go!</AccordionDetails>
+    </Accordion>
+
+    <Accordion>
+      <AccordionSummary  expandIcon={<ExpandMore />}>What can I create?</AccordionSummary>
+      <AccordionDetails>Anything your heart desires! Hardware products, software applications, gaming interfaces ~ if you can dream it, you can build it!</AccordionDetails>
+    </Accordion>
+
+    <Accordion>
+      <AccordionSummary  expandIcon={<ExpandMore />}>How much does it cost?</AccordionSummary>
+      <AccordionDetails>GraceHacks will be absolutely FREE! We’ll provide food, swag, caffeine, WiFi, and a collaborative workspace to get your projects up and running quickly!</AccordionDetails>
+    </Accordion>
     </div>
   </div>
   );
