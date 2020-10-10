7d057bd406d20ef9a51d1ef0ca054c1c3b6e2f07
Sarah Avila <sttavila@gmail.com>
2020-10-09 19:33:55 -0700

    two column implementation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,29 +9,35 @@ function Header() {
   const useStyles = makeStyles((theme) => ({
     register: {
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        // justifyContent: "center",
+        // alignItems: "center",
         fontSize: "25px",
         fontFamily: "inherit",
         color: "#413f3f !important",
         width: "185px",
-        marginLeft: "250px",
+        marginLeft: "20px",
         flexWrap: "wrap"
       }
     }));
     
   return (
     <div class="header-container">
-      <img class="image" src={GraceHacks}></img>
-      <div class="title">
-        <div class="letter">U</div>
-        <div class="letter">C</div>
-        <div class="letter">S</div>
-        <div class="letter">C</div>
-        <div class="gracehacks">GraceHacks</div>
+      <div class="left-column">
+        <img class="image" src={GraceHacks}></img>
+        <div class="title">
+          <div class="letter">U</div>
+          <div class="letter">C</div>
+          <div class="letter">S</div>
+          <div class="letter">C</div>
+        </div>
       </div>
-      <div class="event"> October 17th-18th 11AM PST</div>
-      <Button variant="contained" href="https://tinyurl.com/GH-app" className={useStyles().register}>REGISTER</Button>
+      <div class="right-column">
+        <div class="title">
+          <div class="gracehacks">GraceHacks</div>
+        </div>
+        <div class="event"> October 17th-18th 11AM PST</div>
+        <Button variant="contained" href="https://tinyurl.com/GH-app" className={useStyles().register}>REGISTER</Button>
+      </div>
     </div>
   );
 }
