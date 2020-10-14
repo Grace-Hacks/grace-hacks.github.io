b193059eaeac55ac4b394cfb129beee845c22e64
Tim <tim.nguyen.cs@gmail.com>
2020-10-14 13:41:09 -0700

    inclusive language update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div class="about-container" id="mission">
       <div class="title">Create With Confidence</div>
-      <div class="subtitle">UC Santa Cruz's premier all-female and non-binary hackathon.</div>
+      <div class="subtitle">A hackathon highlighting marginalized genders in S.T.E.M, started by UC Santa Cruz students.</div>
       <div class="description">The aim of GraceHacks is to foster a welcoming and supportive hackathon environment for women and non-binary students. GraceHacks strives to empower these underrepresented groups and ensure that they have the confidence to create without inherent bias or sexism.</div>
     </div>
   );
