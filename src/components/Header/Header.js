62eedd3e7997c5c54f5698961c372e6609d0cf0d
Sarah Avila <sttavila@gmail.com>
2020-10-13 23:33:57 -0700

    fix custom breakpoints and clean up code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,37 +3,36 @@ import './Header.scss';
 import GraceHacks from '../../assets/GraceHacks.png'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 
 function Header() {
 
-  const theme = createMuiTheme({
+  const customTheme = createMuiTheme({
     breakpoints: {
       values: {
         xs: 0,
         sm: 600,
         md: 960,
-        lg: 900,
+        lg: 1100,
         xl: 1920,
       },
     },
   })
 
-  console.log(theme);
+  console.log(customTheme);
 
-  const useStyles = makeStyles((theme) => ({
+  const useStyles = makeStyles(() => ({
     registerDesktop: {
       fontSize: "25px",
       fontFamily: "inherit",
       color: "#413f3f !important",
       width: "185px",
       marginLeft: "21px",
-      [theme.breakpoints.down("lg")]: {
+      [customTheme.breakpoints.down("lg")]: {
           display: "none",
       },
-      [theme.breakpoints.up("lg")]: {
+      [customTheme.breakpoints.up("lg")]: {
         display: "flex",
       },
     },
@@ -41,23 +40,20 @@ function Header() {
       fontSize: "25px",
       fontFamily: "inherit",
       color: "#413f3f !important",
-      width: "340px",
-      marginLeft: "18px",
-      [theme.breakpoints.down("lg")]: {
+      width: "330px",
+      marginLeft: "10px",
+      [customTheme.breakpoints.down("lg")]: {
         display: "flex",
       },
-      [theme.breakpoints.up("lg")]: {
+      [customTheme.breakpoints.up("lg")]: {
         display: "none",
       },
     },
   }));
 
-   const matches = useMediaQuery('(max-width:1100px)');
-
   return (
     <div class="header-container">
       <div class="left-column">
-      {/* <span>{`(min-width:1100px) matches: ${matches}`}</span> */}
         <img class="image" src={GraceHacks}></img>
         <div class="title">
           <div class="letter">U</div>
@@ -69,8 +65,8 @@ function Header() {
       <div class="right-column">
         <div class="gracehacks">GraceHacks</div>
         <div class="event"> October 17th-18th 11AM PST</div>
-        <Button variant="contained" href="https://tinyurl.com/GH-app" className={useStyles(theme).registerDesktop}>REGISTER</Button>
-        <Button variant="contained" href="https://tinyurl.com/GH-app" className={useStyles(theme).registerMobile}>REGISTER</Button>
+        <Button variant="contained" href="https://tinyurl.com/GH-app" className={useStyles().registerDesktop}>REGISTER</Button>
+        <Button variant="contained" href="https://tinyurl.com/GH-app" className={useStyles().registerMobile}>REGISTER</Button>
       </div>
     </div>
   );
