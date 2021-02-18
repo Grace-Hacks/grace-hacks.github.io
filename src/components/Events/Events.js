f2508c018c988042b95aad6d225a1c2b21cfd931
Sarah Avila <sttavila@gmail.com>
2021-02-17 22:12:49 -0800

    added events section

---
+++ src/components/Events/Events.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Countdown from 'react-countdown';
+
+import Keyboard from '../../assets/keyboard.png'
+import Landscape from '../../assets/landscape.png'
+import './Events.scss';
+
+function Events() {
+  return (
+    <div class="events-container" id="events">
+        <div class="word">
+        <div class="letter">E</div>
+        <div class="letter">V</div>
+        <div class="letter">E</div>
+        <div class="letter">N</div>
+        <div class="letter">T</div>
+        <div class="letter">S</div>
+      </div>
+    </div>
+  );
+}
+
+export default Events;
