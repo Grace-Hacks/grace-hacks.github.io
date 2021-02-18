325f7e378611e3a6f0e9f36315b5118590b0b76f
mpadil18 <mpadil18@ucsc.edu>
2021-02-17 17:42:36 -0800

    Update Sponsors, About, NavBar

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,6 @@ function About() {
         <div class="info">
           <div class="title">Create with Confidence</div>
           <div class="description">The aim of GraceHacks is to foster a welcoming and supportive hackathon environment for women and non-binary students. GraceHacks strives to empower these underrepresented groups and ensure that they have the confidence to create without inherent bias or sexism.</div>
-          <div class="title"><Countdown date={"2020-10-17T18:00:00Z"} daysInHours="true" /> Hours</div>
         </div>
       </div>
       <div class="about-section">
