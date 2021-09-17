7ebef53585839f85349be229e98978a0251be55b
Sarah Avila <sttavila@gmail.com>
2021-09-16 21:38:53 -0700

    mobile view draft

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import './Header.scss';
 import Calendar from '../../assets/Calendar.svg'
 import Location from '../../assets/Location.svg'
 import Lotus from '../../assets/Lotus.svg'
+import LotusMobile from '../../assets/Lotus-Mobile.svg'
 import Arrow from '../../assets/Header-Arrow.svg'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -11,39 +12,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 function Header() {
 
-  const customTheme = createMuiTheme({
-    breakpoints: {
-      values: {
-        xs: 0,
-        sm: 600,
-        md: 960,
-        lg: 1110,
-        xl: 1920,
-      },
-    },
-  })
-
-  const useStyles = makeStyles(() => ({
-    register: {
-      fontFamily: "inherit",
-      background: "linear-gradient(180deg, #F2ACAC 0%, #EB84B5 79.69%) !important",
-      textAlign: "center",
-      borderRadius: "20px", 
-      [customTheme.breakpoints.down("lg")]: {
-        fontSize: "18px",
-        width: "330px", 
-        marginLeft: "10px",
-      },
-      [customTheme.breakpoints.up("lg")]: {
-        fontSize: "22px",
-        width: "390px",
-        marginLeft: "21px",
-      },
-    },
-  }));
-
   return (
     <div class="header-container">
+      <img class="lotus-mobile" src={LotusMobile}></img>
       <div class="row">
         <div class="left-column">
           <div class="gracehacks">GraceHacks</div>
@@ -60,7 +31,7 @@ function Header() {
           </Button>
         </div>
         <div class="right-column">
-          <img class="image" src={Lotus}></img>
+          <img class="lotus-desktop" src={Lotus}></img>
         </div>
       </div>
       <img class="image" src={Arrow}></img>
