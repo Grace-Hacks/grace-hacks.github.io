db136cf8d52a579b5e98d7d6cbe1a19360333da4
Sarah Avila <sttavila@gmail.com>
2021-09-28 20:22:30 -0700

    small fixes to about and sponsors section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Countdown from 'react-countdown';
-
-import Keyboard from '../../assets/keyboard.png'
-import Landscape from '../../assets/landscape.png'
 import './About.scss';
 
 function About() {
