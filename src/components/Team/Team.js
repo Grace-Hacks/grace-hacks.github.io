ab609ec24920a021a4dfd602195e700f100eecd4
Maritza Padilla <maritzapadilla@Maritzas-iMac.local>
2022-09-23 21:12:24 -0700

    Created new team member animation for bottom of page. Added some pictures

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,138 +1,63 @@
 import React from 'react';
 import './Team.scss';
-import Saniya from "../../assets/Team/Saniya-min.png";
-import Anu from "../../assets/Team/Anu-min.png"
-import Merilyn from "../../assets/Team/Merilyn-min.png";
-import Sarah from "../../assets/Team/Sarah-min.png";
-import Brina from "../../assets/Team/Brina-min.png";
-import Maritza from "../../assets/Team/Maritza-min.png";
-import Swetha from "../../assets/Team/Swetha-min.png";
-import Kat from "../../assets/Team/Kat-min.png";
-import Ana from "../../assets/Team/Ana-min.png";
-import Venus from "../../assets/Team/Venus-min.png";
-import Ojal from "../../assets/Team/Ojal-min.png";
-import Tresa from "../../assets/Team/Tresa-min.png";
-import logo from "../../assets/logo.png";
-
+import Maritza from "../../assets/Team/Maritza_Padilla.png";
+import Lakshmi from "../../assets/Team/Lakshmi_Damodaran.png";
+import Jonathan from "../../assets/Team/Jonathan_He.png";
+import Merilyn from "../../assets/Team/Merilyn_Kuo.jpg";
+import Ojal from "../../assets/Team/Ojal_Mapuskar.jpg";
+import Shirin from "../../assets/Team/Shirin_Rokni.png";
+import Tanisha from "../../assets/Team/Tanisha_Katoch.png";
 
 function Team() {
   return (
     <div class="team-container">
-      <div class="title">
-      team
-      </div>
+      <div class="marquee">
+        <div class="marquee--inner">
+          <span>
+            <img class="orb" src={Maritza} alt="GraceHacks member" />
+            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
+            <img class="orb" src={Jonathan} alt="GraceHacks member" />
+            <img class="orb" src={Merilyn} alt="GraceHacks member" />
+            <img class="orb" src={Ojal} alt="GraceHacks member" />
+            <img class="orb" src={Shirin} alt="GraceHacks member" />
+            <img class="orb" src={Tanisha} alt="GraceHacks member" />
+            <img class="orb" src={Maritza} alt="GraceHacks member" />
+            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
+            <img class="orb" src={Jonathan} alt="GraceHacks member" />
+            <img class="orb" src={Merilyn} alt="GraceHacks member" />
+            <img class="orb" src={Ojal} alt="GraceHacks member" />
+            <img class="orb" src={Shirin} alt="GraceHacks member" />
+            <img class="orb" src={Tanisha} alt="GraceHacks member" />
+            <img class="orb" src={Maritza} alt="GraceHacks member" />
+            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
+            <img class="orb" src={Jonathan} alt="GraceHacks member" />
+            <img class="orb" src={Merilyn} alt="GraceHacks member" />
+            <img class="orb" src={Ojal} alt="GraceHacks member" />
+            <img class="orb" src={Shirin} alt="GraceHacks member" />
+          </span>
+          <span>
+          <img class="orb" src={Maritza} alt="GraceHacks member" />
+            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
+            <img class="orb" src={Jonathan} alt="GraceHacks member" />
+            <img class="orb" src={Merilyn} alt="GraceHacks member" />
+            <img class="orb" src={Ojal} alt="GraceHacks member" />
+            <img class="orb" src={Shirin} alt="GraceHacks member" />
+            <img class="orb" src={Tanisha} alt="GraceHacks member" />
+            <img class="orb" src={Maritza} alt="GraceHacks member" />
+            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
+            <img class="orb" src={Jonathan} alt="GraceHacks member" />
+            <img class="orb" src={Merilyn} alt="GraceHacks member" />
+            <img class="orb" src={Ojal} alt="GraceHacks member" />
+            <img class="orb" src={Shirin} alt="GraceHacks member" />
+            <img class="orb" src={Tanisha} alt="GraceHacks member" />
+            <img class="orb" src={Maritza} alt="GraceHacks member" />
+            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
+            <img class="orb" src={Jonathan} alt="GraceHacks member" />
+            <img class="orb" src={Merilyn} alt="GraceHacks member" />
+            <img class="orb" src={Ojal} alt="GraceHacks member" />
+            <img class="orb" src={Shirin} alt="GraceHacks member" />
+          </span>
 
-      <div class="row">
-        <div class="member">
-          <img class="picture" src={Saniya} alt="GraceHacks member" />
-          <div class="name">Saniya Doshi</div>
-          <div class="position">Executive Lead</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Kat} alt="GraceHacks member" />
-          <div class="name">Katia Avila</div>
-          <div class="position">Co-Executive Lead</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="member">
-          <img class="picture" src={Sarah} alt="GraceHacks member" />
-          <div class="name">Sarah Avila</div>
-          <div class="position">Tech Lead</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Brina} alt="GraceHacks member" />
-          <div class="name">Brina Bodnar</div>
-          <div class="position">Tech</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Maritza} alt="GraceHacks member" />
-          <div class="name">Maritza Padilla</div>
-          <div class="position">Tech</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Anu} alt="GraceHacks member" />
-          <div class="name">Anupreet Singh</div>
-          <div class="position">Public Relations Lead</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={logo} alt="GraceHacks member" />
-          <div class="name">Fariha Lateef</div>
-          <div class="position">Public Relations</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="member">
-          <img class="picture" src={logo} alt="GraceHacks member" />
-          <div class="name">Jessica Kuang</div>
-          <div class="position">Public Relations</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Ana} alt="GraceHacks member" />
-          <div class="name">Ana Guo</div>
-          <div class="position">Design Lead</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Venus} alt="GraceHacks member" />
-          <div class="name">Venus Ku</div>
-          <div class="position">Design</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={logo} alt="GraceHacks member" />
-          <div class="name">Menaha Gujavarthi</div>
-          <div class="position">Design</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Merilyn} alt="GraceHacks member" />
-          <div class="name">Merilyn Kuo</div>
-          <div class="position">Logistics Lead</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="member">
-          <img class="picture" src={Tresa} alt="GraceHacks member" />
-          <div class="name">Tresa Ignatius</div>
-          <div class="position">Logistics</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={logo} alt="GraceHacks member" />
-          <div class="name">Lakshmi Damodaran</div>
-          <div class="position">Logistics</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Swetha} alt="GraceHacks member" />
-          <div class="name">Swetha Thiru</div>
-          <div class="position">Sponsorship Lead</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={Ojal} lt="GraceHacks member" />
-          <div class="name">Ojal Mapuskar</div>
-          <div class="position">Sponsorship</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
-        </div>
-        <div class="member">
-          <img class="picture" src={logo} lt="GraceHacks member" />
-          <div class="name">Diya Saha</div>
-          <div class="position">Sponsorship</div>
-          {/* <div class="pronouns">she/her/hers</div> */}
         </div>
       </div>
     </div>
