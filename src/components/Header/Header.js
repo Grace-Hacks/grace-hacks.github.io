382238113cf9194edaf34bd1d6709c1b748eb2b6
Ojal M <ojalmaps@gmail.com>
2022-11-07 21:12:20 -0800

    Adding 2 new assets and updating the links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,43 +2,48 @@ import React from 'react';
 import './Header.scss';
 import Calendar from '../../assets/Calendar.svg'
 import Location from '../../assets/Location.svg'
-import Lotus from '../../assets/Lotus.svg'
+// import Lotus from '../../assets/Lotus.svg'
 import LotusMobile from '../../assets/Lotus-Mobile.svg'
 import Arrow from '../../assets/Header-Arrow.svg'
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { createMuiTheme } from '@material-ui/core/styles';
-
+import Lotus from '../../assets/lotuswater.png'
+import Water from '../../assets/waterback.png'
 
 function Header() {
 
   return (
     <div class="header-container">
-      <img class="lotus-mobile" src={LotusMobile}></img>
+      <img class="lotus-mobile" src={Lotus}></img>
       <div class="row">
         <div class="left-column">
           <div class="gracehacks">GraceHacks</div>
+          <div class="description">create with Confidence</div>
           <div class="info-row">
             <img class="image" src={Calendar}></img>
-            <div class="event">November 13-14th</div>
+            <div class="event">November 12-13th, 2022</div>
           </div>
           <div class="info-row">
             <img class="image" src={Location}></img>
-            <div class="event">UCSC Stevenson Event Center</div>
+            <div class="event">UCSC Engineering 2- Room 180</div>
           </div>
           <div class="info-row">
             <div class = "event"> Sign Up </div>
           </div>
 
           <div class="info-row">
-            <Button disableRipple variant="contained" href="https://forms.gle/vE9pmMkqz2szCmFn8" class="button" target="_blank" rel="noopener noreferrer">
-              <div class="button-text" link = "">Mentor</div>
+            <Button disableRipple variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLSd6LCsyvIZ1Ed32deT-elAnIb3W6Vbk2C8OUFzeT-ZCJJzf_Q/viewform" class="button" target="_blank" rel="noopener noreferrer">
+              <div class="button-text" link = "">Register to mentor</div>
             </Button> 
 
-            <Button disableRipple variant="contained" href="https://forms.gle/sGDgRxfqR1Hth3Zu9" class="button" target="_blank" rel="noopener noreferrer">
-              <div class="button-text">Participant</div>
+            <Button disableRipple variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLScWdAMZDQUqz6515dUwd7NLKyGsJWeLaJSl2YCgBzQbjszTJg/viewform" class="button" target="_blank" rel="noopener noreferrer">
+              <div class="button-text">Register to hack</div>
+            </Button> 
+
+            <Button disableRipple variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLSeVc55959_MUgShLuzZjLRLvr0iuiP79s2cUbp333YPCPSQag/viewform" class="button" target="_blank" rel="noopener noreferrer">
+              <div class="button-text">Register to judge</div>
             </Button> 
           </div>
+
         </div>
         <div class="right-column">
           <img class="lotus-desktop"  src={Lotus}></img>
