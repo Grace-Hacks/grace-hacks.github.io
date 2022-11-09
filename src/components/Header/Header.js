5ab25d720f6a32dcbc8636f949d0c44f9153f148
Ojal M <ojalmaps@gmail.com>
2022-11-09 13:09:44 -0800

    Adding styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,26 +13,42 @@ function Header() {
 
   return (
     <div class="header-container">
-      <img class="lotus-mobile" src={Lotus}></img>
-      <div class="row">
-        <div class="left-column">
+      {/* <img class="lotus-mobile" src={Lotus}></img> */}
+      <div class="banner" >
+          {/* <img class="lotus-desktop"  src={Lotus}></img> */}
           <div class="gracehacks">GraceHacks</div>
           <div class="description">create with Confidence</div>
-          <div class="info-row">
+      </div>
+
+      <div class="text-box">
+          <div class="date-row">
             <img class="image" src={Calendar}></img>
             <div class="event">November 12-13th, 2022</div>
           </div>
-          <div class="info-row">
+          <div class="location-row">
             <img class="image" src={Location}></img>
             <div class="event">UCSC Engineering 2- Room 180</div>
           </div>
-          <div class="info-row">
-            <div class = "event"> Sign Up </div>
+
+
+          <div class="buttons-row">
+            <div  href="https://docs.google.com/forms/d/e/1FAIpQLSd6LCsyvIZ1Ed32deT-elAnIb3W6Vbk2C8OUFzeT-ZCJJzf_Q/viewform" class="button" >
+              <div class="button-text">Register to mentor</div>
+            </div> 
+
+            <div href="https://docs.google.com/forms/d/e/1FAIpQLScWdAMZDQUqz6515dUwd7NLKyGsJWeLaJSl2YCgBzQbjszTJg/viewform" class="button" >
+              <div class="button-text">Register to hack</div>
+            </div> 
+
+            <div  href="https://docs.google.com/forms/d/e/1FAIpQLSeVc55959_MUgShLuzZjLRLvr0iuiP79s2cUbp333YPCPSQag/viewform" class="button" >
+              <div class="button-text">Register to judge</div>
+            </div> 
           </div>
 
-          <div class="info-row">
+
+          {/* <div class="buttons-row">
             <Button disableRipple variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLSd6LCsyvIZ1Ed32deT-elAnIb3W6Vbk2C8OUFzeT-ZCJJzf_Q/viewform" class="button" target="_blank" rel="noopener noreferrer">
-              <div class="button-text" link = "">Register to mentor</div>
+              <div class="button-text">Register to mentor</div>
             </Button> 
 
             <Button disableRipple variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLScWdAMZDQUqz6515dUwd7NLKyGsJWeLaJSl2YCgBzQbjszTJg/viewform" class="button" target="_blank" rel="noopener noreferrer">
@@ -42,14 +58,9 @@ function Header() {
             <Button disableRipple variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLSeVc55959_MUgShLuzZjLRLvr0iuiP79s2cUbp333YPCPSQag/viewform" class="button" target="_blank" rel="noopener noreferrer">
               <div class="button-text">Register to judge</div>
             </Button> 
-          </div>
-
+          </div> */}
         </div>
-        <div class="right-column">
-          <img class="lotus-desktop"  src={Lotus}></img>
-        </div>
-      </div>
-      <img class="arrow" src={Arrow}></img>
+       
     </div>
   );
 }
