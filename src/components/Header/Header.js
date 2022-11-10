6527a3d8afe9f9bd37d0cafe426cd87a17b22e0a
Ojal M <ojalmaps@gmail.com>
2022-11-09 16:08:46 -0800

    Adding padding to elements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,11 +13,10 @@ function Header() {
 
   return (
     <div class="header-container">
-      {/* <img class="lotus-mobile" src={Lotus}></img> */}
+      
       <div class="banner" >
-          {/* <img class="lotus-desktop"  src={Lotus}></img> */}
-          <div class="gracehacks">GraceHacks</div>
-          <div class="description">create with Confidence</div>
+        <div class="gracehacks">GraceHacks</div>
+        <div class="description">create with confidence</div>
       </div>
 
       <div class="text-box">
@@ -25,28 +24,13 @@ function Header() {
             <img class="image" src={Calendar}></img>
             <div class="event">November 12-13th, 2022</div>
           </div>
+
           <div class="location-row">
             <img class="image" src={Location}></img>
             <div class="event">UCSC Engineering 2- Room 180</div>
           </div>
 
-
           <div class="buttons-row">
-            <div  href="https://docs.google.com/forms/d/e/1FAIpQLSd6LCsyvIZ1Ed32deT-elAnIb3W6Vbk2C8OUFzeT-ZCJJzf_Q/viewform" class="button" >
-              <div class="button-text">Register to mentor</div>
-            </div> 
-
-            <div href="https://docs.google.com/forms/d/e/1FAIpQLScWdAMZDQUqz6515dUwd7NLKyGsJWeLaJSl2YCgBzQbjszTJg/viewform" class="button" >
-              <div class="button-text">Register to hack</div>
-            </div> 
-
-            <div  href="https://docs.google.com/forms/d/e/1FAIpQLSeVc55959_MUgShLuzZjLRLvr0iuiP79s2cUbp333YPCPSQag/viewform" class="button" >
-              <div class="button-text">Register to judge</div>
-            </div> 
-          </div>
-
-
-          {/* <div class="buttons-row">
             <Button disableRipple variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLSd6LCsyvIZ1Ed32deT-elAnIb3W6Vbk2C8OUFzeT-ZCJJzf_Q/viewform" class="button" target="_blank" rel="noopener noreferrer">
               <div class="button-text">Register to mentor</div>
             </Button> 
@@ -58,9 +42,9 @@ function Header() {
             <Button disableRipple variant="contained" href="https://docs.google.com/forms/d/e/1FAIpQLSeVc55959_MUgShLuzZjLRLvr0iuiP79s2cUbp333YPCPSQag/viewform" class="button" target="_blank" rel="noopener noreferrer">
               <div class="button-text">Register to judge</div>
             </Button> 
-          </div> */}
+          </div>
         </div>
-       
+
     </div>
   );
 }
