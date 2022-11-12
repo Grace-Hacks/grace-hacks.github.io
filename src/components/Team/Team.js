f3efe9a16551c33579ac4c329ff21aecb7b1f1d1
Maritza Padilla <maritzapadilla@Maritzas-iMac.local>
2022-11-11 21:04:22 -0800

    Refactored Object Rendering and Display names

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -16,11 +16,23 @@ import Tanisha from "../../assets/Team/tanisha.png";
 import TBDe from "../../assets/Team/tbd.png";
 import Wing from "../../assets/Team/wing.png";
 
+let members = {'Maritza Padilla':['Co-Exec Lead', 'Tech Lead'], 'Rina Munakata':['Co-Exec Lead'], 'Jonathan He':['Co-Exec Lead', 'Design Lead'], 'Lakshmi Damodaran':['Co-Exec Lead'], 
+'Merilyn Kuo':['Logistics Lead'], 'Anusha Pai':['Logistics'], 'Hersh Kalsi':['Logistics'], 
+'Andrew Sanchez':['Tech'], 'Thanh To':['Tech'], 'Ojal Mapuskar':['Tech'], 'Shirin Rokni':['Public Relations Lead'], 'Ariam Amelash':['Public Relations'],
+'Wing Koon':['Public Relations'], 'Tanisha Katoch':['Sponsorship Lead'], 'Justin Keovongsa':['Sponsorship'], 'Priya Charagondla': ['Design'], 'Sabrina Yone': ['Design']};
+
+let assets = [Maritza, Rina, Jonathan, Lakshmi, Merilyn, Anusha, TBDe, Andrew,
+TBDe, Ojal, Shirin, Ariam, Wing, Tanisha, TBDe, Priya, Sabrina]
 function Team() {
   return (
     <div class="team-container">
       <div class="marquee">
         <div class="marquee--inner">
+          <div class="spin">
+          {assets.map((item, index) => { 
+            return <img class="orb" src={item} alt="GraceHacks member" />
+          })}
+          </div>
           <div class="spin">
             <img class="orb" src={Andrew} alt="GraceHacks member" />
             <img class="orb" src={Anusha} alt="GraceHacks member" />
@@ -44,26 +56,18 @@ function Team() {
             <img class="orb" src={Shirin} alt="GraceHacks member" />
           </div>
           <div class="spin">
-            <img class="orb" src={Andrew} alt="GraceHacks member" />
-            <img class="orb" src={Anusha} alt="GraceHacks member" />
-            <img class="orb" src={Ariam} alt="GraceHacks member" />
-            <img class="orb" src={Jonathan} alt="GraceHacks member" />
-            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
-            <img class="orb" src={Maritza} alt="GraceHacks member" />
-            <img class="orb" src={Merilyn} alt="GraceHacks member" />
-            <img class="orb" src={Ojal} alt="GraceHacks member" />
-            <img class="orb" src={Priya} alt="GraceHacks member" />
-            <img class="orb" src={Rina} alt="GraceHacks member" />
-            <img class="orb" src={Sabrina} alt="GraceHacks member" />
-            <img class="orb" src={Shirin} alt="GraceHacks member" />
-            <img class="orb" src={Tanisha} alt="GraceHacks member" />
-            <img class="orb" src={TBDe} alt="GraceHacks member" />
-            <img class="orb" src={Wing} alt="GraceHacks member" />
-            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
-            <img class="orb" src={Jonathan} alt="GraceHacks member" />
-            <img class="orb" src={Merilyn} alt="GraceHacks member" />
-            <img class="orb" src={Ojal} alt="GraceHacks member" />
-            <img class="orb" src={Shirin} alt="GraceHacks member" />
+          {
+            Object.keys(members).map((key, index) => ( 
+              <span class="member-name"> {key} | {members[key]}</span>
+            ))
+          }
+          </div>
+          <div class="spin">
+          {
+            Object.keys(members).map((key, index) => ( 
+              <span class="member-name"> {key} | {members[key]}</span>
+            ))
+          }
           </div>
 
         </div>
