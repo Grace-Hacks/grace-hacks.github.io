165ea931bee7fa1b78787df2103634aeffe2ee50
Maritza Padilla <maritzapadilla@Maritzas-iMac.local>
2022-11-11 21:09:28 -0800

    Slight Mods

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -13,16 +13,18 @@ import Rina from "../../assets/Team/rina.png";
 import Sabrina from "../../assets/Team/sabrina.png";
 import Shirin from "../../assets/Team/shirin.png";
 import Tanisha from "../../assets/Team/tanisha.png";
+import Thanh from "../../assets/Team/thanh.png";
 import TBDe from "../../assets/Team/tbd.png";
 import Wing from "../../assets/Team/wing.png";
+import Lotus from "../../assets/Lotus.svg";
 
 let members = {'Maritza Padilla':['Co-Exec Lead', 'Tech Lead'], 'Rina Munakata':['Co-Exec Lead'], 'Jonathan He':['Co-Exec Lead', 'Design Lead'], 'Lakshmi Damodaran':['Co-Exec Lead'], 
 'Merilyn Kuo':['Logistics Lead'], 'Anusha Pai':['Logistics'], 'Hersh Kalsi':['Logistics'], 
 'Andrew Sanchez':['Tech'], 'Thanh To':['Tech'], 'Ojal Mapuskar':['Tech'], 'Shirin Rokni':['Public Relations Lead'], 'Ariam Amelash':['Public Relations'],
 'Wing Koon':['Public Relations'], 'Tanisha Katoch':['Sponsorship Lead'], 'Justin Keovongsa':['Sponsorship'], 'Priya Charagondla': ['Design'], 'Sabrina Yone': ['Design']};
 
-let assets = [Maritza, Rina, Jonathan, Lakshmi, Merilyn, Anusha, TBDe, Andrew,
-TBDe, Ojal, Shirin, Ariam, Wing, Tanisha, TBDe, Priya, Sabrina]
+let assets = [Maritza, Rina, Jonathan, Lakshmi, Merilyn, Anusha, Lotus, Andrew,
+Thanh, Ojal, Shirin, Ariam, Wing, Tanisha, Lotus, Priya, Sabrina]
 function Team() {
   return (
     <div class="team-container">
@@ -34,26 +36,9 @@ function Team() {
           })}
           </div>
           <div class="spin">
-            <img class="orb" src={Andrew} alt="GraceHacks member" />
-            <img class="orb" src={Anusha} alt="GraceHacks member" />
-            <img class="orb" src={Ariam} alt="GraceHacks member" />
-            <img class="orb" src={Jonathan} alt="GraceHacks member" />
-            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
-            <img class="orb" src={Maritza} alt="GraceHacks member" />
-            <img class="orb" src={Merilyn} alt="GraceHacks member" />
-            <img class="orb" src={Ojal} alt="GraceHacks member" />
-            <img class="orb" src={Priya} alt="GraceHacks member" />
-            <img class="orb" src={Rina} alt="GraceHacks member" />
-            <img class="orb" src={Sabrina} alt="GraceHacks member" />
-            <img class="orb" src={Shirin} alt="GraceHacks member" />
-            <img class="orb" src={Tanisha} alt="GraceHacks member" />
-            <img class="orb" src={TBDe} alt="GraceHacks member" />
-            <img class="orb" src={Wing} alt="GraceHacks member" />
-            <img class="orb" src={Lakshmi} alt="GraceHacks member" />
-            <img class="orb" src={Jonathan} alt="GraceHacks member" />
-            <img class="orb" src={Merilyn} alt="GraceHacks member" />
-            <img class="orb" src={Ojal} alt="GraceHacks member" />
-            <img class="orb" src={Shirin} alt="GraceHacks member" />
+          {assets.map((item, index) => { 
+            return <img class="orb" src={item} alt="GraceHacks member" />
+          })}
           </div>
           <div class="spin">
           {
