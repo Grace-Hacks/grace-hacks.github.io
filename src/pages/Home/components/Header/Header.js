f02487d59d279059df8e3654c348a2a8857c7b35
audsostrom <audreyostrom64@gmail.com>
2023-09-29 14:14:01 -0700

    acm section added

--- src/pages/Home/components/Header/Header.js
+++ src/pages/Home/components/Header/Header.js
@@ -3,6 +3,7 @@ import './Header.scss';
 import Deer1 from '../../../../assets/Deer1.svg';
 import Deer2 from '../../../../assets/Deer2.svg';
 import Squiggle from '../../../../assets/Squiggle.svg';
+import CollabLogo from '../../../../assets/gracehacks-acm-logo.svg';
 /**
  * Unused Assets
   import Calendar from '../../assets/Calendar.svg'
@@ -19,12 +20,17 @@ function Header() {
 
   return (
     <div className="header-container" id="home">
-      <div className="banner" >
+      <div className="banner">
         <span className="gracehacks">grace<wbr></wbr>hacks</span>
         <div className="description">Santa Cruz's all inclusive hackathon for students of diverse genders.</div>
         <div className="event-info">
-          <div>November 10-12th</div>
-          <div>UCSC Stevenson Event Center</div>
+          <img className='collab-logo' src={CollabLogo}/>
+          <div className='collab-time'>November 10-12th @ Location TBA</div>
+          <div className='collab-description'>
+            We are excited to partner with ACM to host a large-scale hackathon 
+            to offer a variety of opportunities to explore STEM fields and build 
+            technical skills through diverse workshops and networking opportunities.
+          </div>
         </div>
       </div>
       <div className="curve"></div>
